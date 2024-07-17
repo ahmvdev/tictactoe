19da7c8247c9f0248bacc95249ddd688c9802b74
candy <abdullahbasir324@gmail.com>
2024-07-17 08:14:27 +0500

    added html and css plus button logs out working

---
+++ main.js
@@ -0,0 +1,18 @@
+const box = document.querySelector('.box');
+const restart = document.querySelector('#restart')
+
+
+
+restart.addEventListener('click', function(){
+    console.log('button working')
+ });
+const gameboard = function (){
+    const board = ['', '', '', '', '', '', '', '', ''];
+
+
+
+}
+
+
+
+gameboard();
