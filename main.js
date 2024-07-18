9827bd1a8337d61ac2c5bf1938ae13853b79163e
candy <abdullahbasir324@gmail.com>
2024-07-18 15:09:18 +0500

    starting to understand a little bit

--- main.js
+++ main.js
@@ -5,10 +5,25 @@ const gameBoard = function (){
     const restart = document.querySelector('#restart');
     const board = ['', '', '', '', '', '', '', '', ''];
 
+const Player = function(name, marker){
+    return {
+        name,
+        marker
+    }
+}
+
+const playerCreate = function(){
+        const playerX = Player('P1', 'X');
+        const playerO = Player('P2', 'O');
+
+        return{playerX, playerO};
+}();
+
+
 const checkCell = function (){
     cells.forEach((index) =>{
         index.addEventListener('click', function(){
-            console.log('cell clicked')
+           console.log('button clicked');
         })
     }
 
