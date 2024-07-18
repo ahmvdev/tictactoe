1a0111372d2adab66c779721c8e5ba1298054fdd
candy <abdullahbasir324@gmail.com>
2024-07-18 14:10:58 +0500

    added check for each cell

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
-function Gameboard(){
+const gameBoard = function (){
 
-const cells = document.querySelector('.box');
-const restart = document.querySelector('#restart')
-
-
-const gameboard = function (){
+    const cells = document.querySelectorAll('.box');
+    const restart = document.querySelector('#restart');
     const board = ['', '', '', '', '', '', '', '', ''];
 
+const checkCell = function (){
+    cells.forEach((index) =>{
+        index.addEventListener('click', function(){
+            console.log('cell clicked')
+        })
+    }
 
-
-}
-}
-
-
-gameboard();
+    )
+}();
+}();
