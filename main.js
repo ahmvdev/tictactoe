95d5df3ca3068d37d9dfeb7f74b7de7f86b9fab2
candy <abdullahbasir324@gmail.com>
2024-07-18 13:02:56 +0500

    fixed variable name

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const box = document.querySelector('.box');
+
+function Gameboard(){
+
+const cells = document.querySelector('.box');
 const restart = document.querySelector('#restart')
 
 
-
-restart.addEventListener('click', function(){
-    console.log('button working')
- });
 const gameboard = function (){
     const board = ['', '', '', '', '', '', '', '', ''];
 
 
 
 }
-
+}
 
 
 gameboard();
